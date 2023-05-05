5b4a05ac5b5a39e451e9fec63c6d76afeda1e4fb
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2023-05-05 16:00:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const display = document.querySelector('.display');
+const startBtn = document.querySelector('.stsrt');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+
+let startTime, intervalId;
+
+//Define the start function that Starts Time:
+
+function start(){ 
+    startTime = Date.now();
+    intervalId = setInterval(() =>{
+        const elapsedTime = Date.now() - startTime;
+        const seconds = Math.floor(elapsedTime/1000);
+        const minutes = Math.floor(seconds/60);
+        const hours = Math.floor(minutes/60);
+
+        // HH:MM:SS
+        const formattedTime = `${pad(hours)}: ${pad(minutes)}: 
+        ${pad(seconds)}`
+
+        display.textContent = formattedTime;
+
+    }, 10);
+}
+
+function stop (){
+    clearInterval(intervalId);
+}
+
+function reset (){
+    clearInterval(intervalId);
+    display.textContent ='00:00:00';
+
+}
+
+// adding 0 to nos. for 0-9
+function pad(number){
+    return number < 10 ? '0' + number : number;
+}
+
+startBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);
+resetBtn.addEventListener('click', reset);
+
